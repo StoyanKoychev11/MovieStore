166d416bd2ecadc13ce8a9096fcb82144069c119
Tyurkyan <turkanmollahasan@gmail.com>
2024-11-16 10:32:36 +0200

    init

---
+++ MovieStore-main/MovieStore-main/MovieStore.DL/Repositories/MovieRepository.cs
@@ -0,0 +1,14 @@
+﻿using MovieStore.DL.DB;
+using MovieStore.DL.Interfaces;
+using MovieStore.Models.DTO;
+
+namespace MovieStore.DL.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public List<Movie> GetMovies()
+        {
+            return StaticData.Movies;
+        }
+    }
+}
