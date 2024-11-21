92f581ff83bb1e7bcd1a8858a71d4afe4947d0a7
StoyanKoychev11 <skoychev3@abv.bg>
2024-11-21 05:56:31 -0800

    Init

--- MovieStore-main/MovieStore-main/MovieStore/Controllers/MoviesController.cs
+++ MovieStore-main/MovieStore-main/MovieStore/Controllers/MoviesController.cs
@@ -1,24 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MovieStore.DL.Interfaces;
-using MovieStore.Models.DTO;
-namespace MovieStore.Controllers
+using Microsoft.AspNetCore.Mvc;
+using MovieStoreB.BL.Interfaces;
+using MovieStoreB.Models.DTO;
+
+namespace MovieStoreB.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class MoviesController : ControllerBase
     {
-        private readonly IMovieRepository _movieRepository;
+        private readonly IMovieService _movieService;
 
-        public MoviesController(
-            IMovieRepository movieRepository)
+        public MoviesController(IMovieService movieService)
         {
-            _movieRepository = movieRepository;
+            _movieService = movieService;
         }
 
-        [HttpGet]
-        public IEnumerable<Movie> Get()
+        [HttpGet("GetAll")]
+        public IEnumerable<Movie> GetAll()
         {
-            return _movieRepository.GetMovies();
+            return _movieService.GetMovies();
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetById(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            var result =
+                _movieService.GetMoviesById(id);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+        [HttpPost("AddMovie")]
+        public void AddMovie([FromBody]Movie movie)
+        {
+            _movieService.AddMovie(movie);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0) return BadRequest($"Wrong id:{id}");
+
+            _movieService.DeleteMovie(id);
+
+            return Ok();
         }
     }
 }
