df5067c16d697dcea09ec40a4c0ecc4c7d50fde1
StoyanKoychev11 <skoychev3@abv.bg>
2024-12-05 13:19:55 -0800

    init

---
+++ MovieStore-main/MovieStore-main/MovieStore.Models/Responses/FullMovieDetails.cs
@@ -0,0 +1,15 @@
+﻿using MovieStore.Models.DTO;
+
+namespace MovieStore.Models.Responses
+{
+    public class FullMovieDetails
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public List<Actor> Actors { get; set; }
+    }
+}
