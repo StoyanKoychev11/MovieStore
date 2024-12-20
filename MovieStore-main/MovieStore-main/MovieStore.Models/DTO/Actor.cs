53329063006b6ad4a1bab902dc36732a7ad671f6
StoyanKoychev11 <skoychev3@abv.bg>
2024-12-20 15:36:53 +0200

    Init

--- MovieStore-main/MovieStore-main/MovieStore.Models/DTO/Actor.cs
+++ MovieStore-main/MovieStore-main/MovieStore.Models/DTO/Actor.cs
@@ -1,9 +1,9 @@
-﻿namespace MovieStore.Models.DTO
+﻿namespace MovieStoreB.Models.DTO
 {
     public class Actor
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         public string Name { get; set; }
     }
-}
+ }
