08617cbf9ed58be29e01daf4fa23e36bd1982ddc
StoyanKoychev11 <skoychev3@abv.bg>
2024-12-20 15:39:46 +0200

    Init

--- MovieStore-main/MovieStore-main/MovieStore.Models/Request/AddMovieRequest.cs
+++ MovieStore-main/MovieStore-main/MovieStore.Models/Request/AddMovieRequest.cs
@@ -1,9 +1,11 @@
-﻿namespace MovieStore.Models.Requests
+﻿namespace MovieStoreB.Models.Requests
 {
     public class AddMovieRequest
     {
         public string Title { get; set; }
 
         public int Year { get; set; }
+
+        public List<string> ActorIds { get; set; }
     }
-}
+ }
