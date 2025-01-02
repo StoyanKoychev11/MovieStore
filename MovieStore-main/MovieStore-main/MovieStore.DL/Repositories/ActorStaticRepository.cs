756560aeb77d08120cd1c5f106c485cfcfe8d64d
StoyanKoychev11 <skoychev3@abv.bg>
2025-01-02 05:02:31 -0800

    mongo

--- MovieStore-main/MovieStore-main/MovieStore.DL/Repositories/ActorStaticRepository.cs
+++ MovieStore-main/MovieStore-main/MovieStore.DL/Repositories/ActorStaticRepository.cs
@@ -1,14 +1,14 @@
-﻿using MovieStore.DL.DB;
+﻿﻿using MovieStore.DL.DB;
 using MovieStore.DL.Interfaces;
 using MovieStore.Models.DTO;
 
 namespace MovieStore.DL.Repositories
 {
-    public class ActorStaticRepository : IActorRepository
-    {
-        public Actor? GetById(int id)
-        {
-            return StaticData.Actors.FirstOrDefault(a => a.Id == id);
-        }
-    }
+    //public class ActorStaticRepository : IActorRepository
+    //{
+    //    public Actor? GetById(int id)
+    //    {
+    //        return StaticData.Actors.FirstOrDefault(a => a.Id == id);
+    //    }
+    //}
 }
