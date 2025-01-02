33b47d15e5189196794a597ba8eaa092a7960bf4
StoyanKoychev11 <skoychev3@abv.bg>
2025-01-02 04:52:12 -0800

    mongo

---
+++ MovieStore-main/MovieStore-main/MovieStore.BL/Service/MovieService.cs
@@ -0,0 +1,50 @@
+﻿using MovieStoreB.BL.Interface;
+using MovieStoreB.DL.Interfaces;
+using MovieStoreB.Models.DTO;
+
+namespace MovieStoreB.BL.Service
+{
+    internal class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _movieRepository;
+
+        public MovieService(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return _movieRepository.GetMovies();
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            if (movie == null || movie.Actors == null) return;
+
+            foreach (var actor in movie.Actors)
+            {
+                if (!Guid.TryParse(actor, out _)) return;
+            }
+
+            _movieRepository.AddMovie(movie);
+        }
+
+        public void DeleteMovie(string id)
+        {
+            if (!string.IsNullOrEmpty(id)) return;
+
+            _movieRepository.DeleteMovie(id);
+        }
+
+        public Movie? GetMoviesById(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+
+            return _movieRepository.GetMoviesById(id);
+        }
+    }
+}
