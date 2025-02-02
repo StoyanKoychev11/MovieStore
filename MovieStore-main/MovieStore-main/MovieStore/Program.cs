9287baeb2aec378f76c9144d52c890b177c42c51
StoyanKoychev11 <skoychev3@abv.bg>
2025-02-02 21:04:25 +0200

    init

--- MovieStore-main/MovieStore-main/MovieStore/Program.cs
+++ MovieStore-main/MovieStore-main/MovieStore/Program.cs
@@ -1,51 +1,61 @@
-using FluentValidation.AspNetCore;
 using FluentValidation;
+using FluentValidation.AspNetCore;
 using Mapster;
 using MovieStore.BL;
+using MovieStore.BL.Interfaces;
 using MovieStore.DL;
-using Serilog;
-using Serilog.Sinks.SystemConsole.Themes;
-using MovieStore.Controllers;
-using MovieStore.ServiceExtensions;
+using MovieStore.Models.Configurations;
+using MovieStore.Validators;
 
-var builder = WebApplication.CreateBuilder(args);
-
-var logger = new LoggerConfiguration()
-    .Enrich.FromLogContext()
-    .WriteTo.Console(theme:
-        AnsiConsoleTheme.Code)
-    .CreateLogger();
-
-builder.Logging.AddSerilog(logger);
-
-// Add services to the container.
-builder.Services
-    .AddConfigurations(builder.Configuration)
-    .AddDataDependencies()
-    .AddBusinessDependencies();
-
-builder.Services.AddMapster();
-
-builder.Services.AddValidatorsFromAssemblyContaining<TestRequest>();
-builder.Services.AddFluentValidationAutoValidation();
-
-builder.Services.AddControllers();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
+namespace MovieStore
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            //Add configurations
+            builder.Services.Configure<MongoDbConfiguration>(
+                builder.Configuration
+                    .GetSection(nameof(MongoDbConfiguration)));
+
+            // Add services to the container.
+            builder.Services
+                .RegisterRepositories()
+                .RegisterServices();
+
+            builder.Services.AddMapster();
+
+            builder.Services.AddControllers();
+
+            builder.Services
+                .AddValidatorsFromAssemblyContaining<TestValidator>();
+            builder.Services.AddFluentValidationAutoValidation();
+
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddHealthChecks();
+
+            var app = builder.Build();
+
+            app.MapHealthChecks("/healthz");
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+
+            // Configure the HTTP request pipeline.
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
 }
-
-// Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
