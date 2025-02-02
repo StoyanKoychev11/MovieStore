44e2bf079b815699858d6b83c363b746f2f3fd0d
StoyanKoychev11 <skoychev3@abv.bg>
2025-02-02 20:43:21 +0200

    mongo

--- MovieStore-main/MovieStore-main/MovieStore.Models/DTO/Actor.cs
+++ MovieStore-main/MovieStore-main/MovieStore.Models/DTO/Actor.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MovieStore.Models.DTO
+﻿namespace MovieStore.Models.DTO
 {
     public class Actor
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public string Id { get; set; }
 
+        public string Name { get; set; }
     }
 }
