4aff19f1fed91124918fc85bc5684e39476590fb
StoyanKoychev11 <skoychev3@abv.bg>
2025-02-02 20:46:20 +0200

    mongo

--- MovieStore-main/MovieStore-main/MovieStore.Models/Request/AddMovieRequest.cs
+++ MovieStore-main/MovieStore-main/MovieStore.Models/Request/AddMovieRequest.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MovieStore.Models.Requests
+﻿namespace MovieStore.Models.Requests
 {
     public class AddMovieRequest
     {
-        public required string Title { get; set; }
+        public string Title { get; set; } = string.Empty;
 
         public int Year { get; set; }
     }
