d41f13528c72133598117b1b2bfc0e1b75ba2f9b
StoyanKoychev11 <skoychev3@abv.bg>
2025-02-02 20:34:49 +0200

    mongo

---
+++ MovieStore-main/MovieStore-main/MovieStore.DL/DependenciesInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MovieStore.DL.Interfaces;
+using MovieStore.DL.Repositories;
+using MovieStore.DL.Repositories.MongoDb;
+
+namespace MovieStore.DL
+{
+    public static class DependenciesInjection
+    {
+        public static IServiceCollection 
+            RegisterRepositories(this IServiceCollection services)
+        {
+            return
+                services
+                    .AddSingleton<IMovieRepository,
+                        MoviesMongoRepository>()
+                    .AddSingleton<IActorRepository,
+                        ActorRepository>();
+        }
+    }
+}
